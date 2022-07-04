cdfb17a6e941d3d1c719e483b36745cf2da57b95
Jordan <jordanb03@protonmail.com>
2022-07-04 17:51:22 +0200

    task3

--- st10036997/src/main/java/com/mycompany/st10036997/St10036997.java
+++ st10036997/src/main/java/com/mycompany/st10036997/St10036997.java
@@ -14,33 +14,41 @@ import javax.swing.JOptionPane;
 public class St10036997 {
 
     public static void main(String[] args) {
-        String username = JOptionPane.showInputDialog(null,"Please enter your username");
-        String password = JOptionPane.showInputDialog(null,"Please enter your password");
-        String name = JOptionPane.showInputDialog(null,"Enter your first name");
-        String surname = JOptionPane.showInputDialog(null,"Enter your surname");
+        String username = JOptionPane.showInputDialog(null, "Please enter your username");
+        String password = JOptionPane.showInputDialog(null, "Please enter your password");
+        String name = JOptionPane.showInputDialog(null, "Enter your first name");
+        String surname = JOptionPane.showInputDialog(null, "Enter your surname");
         boolean quitFlag = false;
 
         if (Login.registerUser(username, password).equals("Successful registration")) {
             JOptionPane.showMessageDialog(null, "Successful registration");
-            if (Login.loginUser(username, password)){
-                JOptionPane.showMessageDialog(null, "Welcome " +name + " " +surname + " it is great to see you again. ");
+            if (Login.loginUser(username, password)) {
+                JOptionPane.showMessageDialog(null,
+                        "Welcome " + name + " " + surname + " it is great to see you again. ");
                 JOptionPane.showMessageDialog(null, "Welcome to EasyKanban");
-                while(!quitFlag){
-                Integer choice = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter 1 to add a task \n2 to show report(Work in progress)\n3 to quit the program"));
-                switch (choice) {
-                    case 1: Task.taskArrays();break;
-                    case 2: JOptionPane.showMessageDialog(null, "This feature is coming soon");break;
-                    case 3: quitFlag = true;break;
-                    default: System.out.println("incorrect input");break;
-                }
-                
-                        
-                }
-                }
-            
+                while (!quitFlag) {
+                    Integer choice = Integer.parseInt(JOptionPane.showInputDialog(null,
+                            "Enter 1 to add a task \n2 to show report\n3 to quit the program"));
+                    switch (choice) {
+                        case 1:
+                        Task2.taskArrays();
+                            
+                            break;
+                        case 2:
+                            
+                            break;
+                        case 3:
+                            quitFlag = true;
+                            break;
+                        default:
+                            System.out.println("incorrect input");
+                            break;
+                    }
 
-        }
-        else {
+                }
+            }
+
+        } else {
             JOptionPane.showMessageDialog(null, Login.registerUser(username, password));
         }
     }
--- st10036997/src/main/java/com/mycompany/st10036997/Task2.java
+++ st10036997/src/main/java/com/mycompany/st10036997/Task2.java
@@ -6,15 +6,16 @@ import scala.collection.mutable.StringBuilder;
 
 import java.util.stream.*;
 
-public class Task {
+public class Task2 {
     public static void taskArrays() {
         int hours=0;
         String taskDescriptionCheck;
+        int sum = 0;
         int taskNumber = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the number of tasks"));
         int[] taskNumArray = new int[taskNumber];
         StringBuilder[] taskName = new String[taskNumber];
         String[] taskDescription = new String[taskNumber];
-        int[] taskTime = new int[taskNumber];
+        String[] taskTime = new String[taskNumber];
         String[] taskStatus = new String[taskNumber];
         String[] taskDeveloper = new String[taskNumber];
 
@@ -26,12 +27,23 @@ public class Task {
                 taskDescriptionCheck = JOptionPane.showInputDialog(null, "Enter a task description that is less than 50 characters");
             }while(!checkTaskDescription(taskDescriptionCheck));
             taskDescription[y] = taskDescriptionCheck;
-            taskTime[y] = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the time in hours to complete the task"));
+            taskTime[y] = JOptionPane.showInputDialog(null, "Enter the time in hours to complete the task");
             taskStatus[y] = JOptionPane.showInputDialog(null, "Task status: \nEnter \"To Do\"\nEnter \"Doing\"\nEnter \"Done\"");
+<<<<<<< Updated upstream:st10036997/src/main/java/com/mycompany/st10036997/Task.java
             taskDeveloper[y] = JOptionPane.showInputDialog(null, "Enter the task name");
             printTaskDetails(taskNumArray[y], taskName[y], taskDescription[y], taskTime[y], taskStatus[y], taskDeveloper[y]);
         }
 
+=======
+            taskDeveloper[y] = JOptionPane.showInputDialog(null, "Enter the task developer");
+            taskID[y] = TaskId(taskName[y], taskNumArray[y], taskDeveloper[y]);
+            JOptionPane.showMessageDialog(null, printTaskDetails(taskNumArray[y], taskName[y], taskDescription[y], taskTime[y], taskStatus[y], taskDeveloper[y], taskID[y])); 
+            
+        }
+        JOptionPane.showMessageDialog(null, "The total hours are: " + returnTotalHours(taskTime, sum));
+        Report.makeReport(taskName, taskDescription, taskTime, taskStatus, taskDeveloper, taskID);
+         
+>>>>>>> Stashed changes:st10036997/src/main/java/com/mycompany/st10036997/Task2.java
 
         
     }
@@ -50,15 +62,27 @@ public class Task {
         return taskName.substring(0,2) + ":" + taskNumber + ":" + taskDeveloper.substring(taskDeveloper.length() - 3);
         
     }
+<<<<<<< Updated upstream:st10036997/src/main/java/com/mycompany/st10036997/Task.java
     public static String printTaskDetails(int number, String name, String taskDescription, int taskTime, String taskStatus, String taskDeveloper) {
         return "Task number is:" + number + "\nTask name is:" + name + "\nTask description is:" + taskDescription + "\nTask time is:" + taskTime + "\nTask status is:" + taskStatus + "\nTask developer is:" + taskDeveloper;
     }
     public static int returnTotalHours(int hours[]) {
         int totalHours = IntStream.of(hours).sum();
         return totalHours;
+=======
+    public static String printTaskDetails(int number, String name, String taskDescription, String taskTime, String taskStatus, String taskDeveloper, String taskID) {
+        return "Task number is: " + number + "\nTask name is: " + name + "\nTask description is: " + taskDescription + "\nTask time is: " + taskTime + "\nTask status is: " + taskStatus + "\nTask developer is: " + taskDeveloper + "\nTask ID is: " + taskID;
+    }
+    public static int returnTotalHours(String [] taskTime, int sum) {
+        for (int i = 0; i < taskTime.length; i++) {
+            sum = sum + Integer.parseInt(taskTime[i]);
+        }
+        return sum;
+
+>>>>>>> Stashed changes:st10036997/src/main/java/com/mycompany/st10036997/Task2.java
     }
 
-        
+
     }
     
 
